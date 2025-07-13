00821e64cc8ba2905b702781d9099ecc79f1fd56
Amir Riaz <amir.riaz.dwh@gmail.com>
2025-07-13 13:35:01 +0400

    fixing the memory issue

--- rstudio/R4.5.1/docker_parts/05_set_r_memory.sh
+++ rstudio/R4.5.1/docker_parts/05_set_r_memory.sh
@@ -1,19 +1,38 @@
 #!/bin/bash
 
-# Dynamically set R_MAX_VSIZE based on available container memory
+# Try cgroup v1
 if [ -f /sys/fs/cgroup/memory/memory.limit_in_bytes ]; then
     CONTAINER_MEM=$(($(cat /sys/fs/cgroup/memory/memory.limit_in_bytes)/1024/1024))
-    R_MEM=$((${CONTAINER_MEM}*80/100))
+
+# Try cgroup v2
+elif [ -f /sys/fs/cgroup/memory.max ]; then
+    MEM_RAW=$(cat /sys/fs/cgroup/memory.max)
+    if [ "$MEM_RAW" != "max" ]; then
+        CONTAINER_MEM=$(($MEM_RAW/1024/1024))
+    fi
+fi
+
+# Fallback: use host system memory
+if [ -z "$CONTAINER_MEM" ] && command -v free &> /dev/null; then
+    CONTAINER_MEM=$(free -m | awk '/^Mem:/ { print $2 }')
+fi
+
+if [ -n "$CONTAINER_MEM" ]; then
+    R_MEM=$(($CONTAINER_MEM * 80 / 100))
     TIMESTAMP=$(date +"%Y-%m-%d %H:%M:%S")
 
-    echo "[$TIMESTAMP] [R_PROFILE] Setting R_MAX_VSIZE=${R_MEM}M based on container memory limit" | tee -a /var/log/r_profile_memory.log >&2
+    echo "[$TIMESTAMP] [R_PROFILE] Setting R_MAX_VSIZE=${R_MEM}M based on memory" | tee -a /var/log/r_profile_memory.log >&2
 
     if ! grep -q "R_MAX_VSIZE=" /usr/local/lib/R/etc/Renviron.site; then
         echo "R_MAX_VSIZE=${R_MEM}M" >> /usr/local/lib/R/etc/Renviron.site
     fi
+
+    echo ""
+    echo "🔧 [INFO] R_MAX_VSIZE has been set to ${R_MEM}M (80% of available memory)"
+    echo ""
 fi
 
-# Rotate the log if it exceeds 1MB
+# Rotate log if over 1MB
 if [ -f /var/log/r_profile_memory.log ] && [ $(stat -c%s /var/log/r_profile_memory.log) -ge 1048576 ]; then
     mv /var/log/r_profile_memory.log /var/log/r_profile_memory.log.old
 fi
